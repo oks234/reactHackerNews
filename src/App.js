aec1974c2169e44851f7444b968e57a3ced0335c
oks4work <oks4work@gmail.com>
2019-07-08 17:44:11 +0900

    developed ~3.10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '10';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const largeColumn = {
 	width: '40%',
@@ -23,30 +25,55 @@ const smallColumn = {
 
 
 class App extends Component {
+  _isMounted = false;
+
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			result: null,
-			searchTerm: DEFAULT_QUERY,
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null,
 		};
 
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
 		this.setSearchTopStories = this.setSearchTopStories.bind(this);
 		this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
 		this.onSearchChange = this.onSearchChange.bind(this);
 		this.onSearchSubmit = this.onSearchSubmit.bind(this);
 		this.onDismiss = this.onDismiss.bind(this);
-	}
+  }
+  
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
 
 	setSearchTopStories(result) {
-		this.setState({ result });
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+		const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+		this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
 	}
 
-	fetchSearchTopStories(searchTerm) {
-		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-			.then(response => response.json())
-			.then(result => this.setSearchTopStories(result))
-			.catch(error => error);
+	fetchSearchTopStories(searchTerm, page = 0) {
+		axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+			.then(result => this._isMounted && this.setSearchTopStories(result.data))
+			.catch(error => this._isMounted && this.setState({ error }));
 	}
 
 	onSearchChange(event) {
@@ -54,9 +81,13 @@ class App extends Component {
 	}
 
 	onSearchSubmit(event) {
-		console.log(event);
 		const { searchTerm } = this.state;
-		this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+
 		event.preventDefault();
 	}
 
@@ -69,15 +100,38 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		const { searchTerm } = this.state;
+    this._isMounted = true;
+
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
 		this.fetchSearchTopStories(searchTerm);
-	}
+  }
+  
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
 	render() {
 		const {
-			result,
-			searchTerm
-		} = this.state;
+			searchTerm,
+      results,
+      searchKey,
+      error
+    } = this.state;
+    
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
+    console.log({ results });
 
 		return (
 			<div className="page">
@@ -90,12 +144,20 @@ class App extends Component {
 						Search
 					</Search>
 				</div>
-				{ result &&
-					<Table
-						list={result.hits}
-						onDismiss={this.onDismiss}
-					/>
-				}
+        { error
+          ? <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>
+          : <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
+				<div className="interactions">
+					<Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+						More
+					</Button>
+				</div>
 			</div>
 		);
 	}
